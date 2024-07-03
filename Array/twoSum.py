4d5fe68ab347c4d4f83443c2be3ee0097c23f99a
NituBfit <nitu.rana@spectacom.in>
2024-07-03 18:07:26 +0530

    solve the two sum problem

---
+++ Array/twoSum.py
@@ -0,0 +1,39 @@
+'''
+Problem Statement: Given an array of integers arr[] and an integer target.
+
+1st variant: Return YES if there exist two numbers such that their sum is equal to the target. Otherwise, return NO.
+
+2nd variant: Return indices of the two numbers such that their sum is equal to the target. Otherwise, we will return {-1, -1}.
+
+Note: You are not allowed to use the same element twice. Example: If the target is equal to 6 and num[1] = 3, then nums[1] + nums[1] = target is not a solution.
+
+Examples:
+
+Example 1:
+Input Format: N = 5, arr[] = {2,6,5,8,11}, target = 14
+Result: YES (for 1st variant)
+       [1, 3] (for 2nd variant)
+Explanation: arr[1] + arr[3] = 14. So, the answer is “YES” for the first variant and [1, 3] for 2nd variant.
+
+Example 2:
+Input Format: N = 5, arr[] = {2,6,5,8,11}, target = 15
+Result: NO (for 1st variant)
+	[-1, -1] (for 2nd variant)
+Explanation: There exist no such two numbers whose sum is equal to the target.
+'''
+
+
+def twoSum(arr, sum):
+    pre_ele_map = {}
+    for i, value in enumerate(arr):
+        diff = sum - value
+        if diff in pre_ele_map:
+            return [pre_ele_map[diff], i]
+        else:
+            pre_ele_map[value] = i
+    return[-1, -1]
+
+arr = [2,6,5,8,11]
+sum = 14
+res = twoSum(arr, sum)
+print("--------------- res :", res)
