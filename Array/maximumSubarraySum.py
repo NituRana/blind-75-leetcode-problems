9fb1fe16cb44dc948e211fb26bd00762cea5e968
NituBfit <nitu.rana@spectacom.in>
2024-07-06 21:05:28 +0530

    lil better approach

--- Array/maximumSubarraySum.py
+++ Array/maximumSubarraySum.py
@@ -13,25 +13,48 @@ Output:
 
 
 '''
+# Approaches to Solve the Problem
+
+
+# 1. Brute Force Approach
 
 arr = [-2,1,-3,4,-1,2,1,-5,4]
 
-def max_sum_sub_array(arr):
+# def max_sum_sub_array(arr):
+#     n = len(arr)
+#     max_sum = arr[0]
+#     start = end = 0
+
+#     for i in range(n):
+#         for j in range(i + 1, n):
+#             current_sum = sum(arr[i:j+1])
+#             if current_sum > max_sum:
+#                 max_sum = current_sum
+#                 start, end = i, j
+#     print(f"------------- sub array : {arr[start:end+1]}")
+#     return max_sum
+
+# res = max_sum_sub_array(arr)
+# print("------------- res :", res)
+
+
+def max_subarray_sum(arr):
     n = len(arr)
-    max_sum = arr[0]
+    max_sum = float('-inf')
     start = end = 0
 
     for i in range(n):
-        for j in range(i + 1, n):
-            current_sum = sum(arr[i:j+1])
+        current_sum = 0
+        for j in range(i, n):
+            current_sum += arr[j]
             if current_sum > max_sum:
                 max_sum = current_sum
                 start, end = i, j
-    print(f"------------- sub array : {arr[start:end+1]}")
+
+    print(f"Subarray: {arr[start:end+1]}")
     return max_sum
 
-res = max_sum_sub_array(arr)
-print("------------- res :", res)
 
+print(max_subarray_sum(arr))
 
 
