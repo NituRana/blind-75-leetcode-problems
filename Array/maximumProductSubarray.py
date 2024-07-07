af51ff087a1734fb0f1c2903aa1fb5a6f6fa502f
NituBfit <nitu.rana@spectacom.in>
2024-07-07 23:35:21 +0530

    solve the maximum product value by usinf brute force approach

---
+++ Array/maximumProductSubarray.py
@@ -0,0 +1,39 @@
+'''
+
+Problem Statement: Given an array that contains both negative and positive integers, find the maximum product subarray.
+
+Examples
+Example 1:
+Input:
+
+ Nums = [1,2,3,4,5,0]
+Output:
+
+ 120
+Explanation:
+
+ In the given array, we can see 1×2×3×4×5 gives maximum product value.
+
+Example 2:
+Input:
+ Nums = [1,2,-3,0,-4,-5]
+Output:
+
+ 20
+Explanation:
+
+ In the given array, we can see (-4)×(-5) gives maximum product value.
+
+'''
+
+nums = [1,2,-3,0,-4,-5]
+def maximum_product_value(arr):
+    result = 0
+    n = len(arr)
+    for i in range(n):
+        for j in range(i+1, n):
+            current_product = arr[i] * arr[j]
+            result = max(current_product, result)
+    return result
+
+print("---Maximum Product Value : ", maximum_product_value(nums))
