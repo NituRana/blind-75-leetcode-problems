910c30d78aa31954297343cdaea9e260f29b578b
NituBfit <nitu.rana@spectacom.in>
2024-07-08 23:39:49 +0530

    search in sorted array with linear search

---
+++ Array/minimumInRotatedSortedArray.py
@@ -0,0 +1,25 @@
+'''
+
+Find Minimum in Rotated Sorted Array
+Suppose an array of length n sorted in ascending order is rotated between 1 and n times. For example, the array nums = [0,1,2,4,5,6,7] might become:
+
+[4,5,6,7,0,1,2] if it was rotated 4 times.
+[0,1,2,4,5,6,7] if it was rotated 7 times.
+
+Notice that rotating an array [a[0], a[1], a[2], ..., a[n-1]] 1 time results in the array [a[n-1], a[0], a[1], a[2], ..., a[n-2]].
+
+Given the sorted rotated array nums of unique elements, return the minimum element of this array.
+
+'''
+# Brute force approach linear search: 
+
+def findMin(nums):
+    min_element = nums[0]
+    n = len(nums)
+    for i in range(n):
+        for j in range(i+1, n):
+            if nums[j] < min_element:
+                min_element = nums[j]
+    return min_element
+arr = [4,5,6,7,0,1,2]
+print("------------------ minimum element :", findMin(arr))
