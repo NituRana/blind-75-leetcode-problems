2a83d38f4dd12e29cba555ee4a096b83e7da7de1
NituBfit <nitu.rana@spectacom.in>
2024-07-21 23:09:07 +0530

    better approach for missing element in array

--- Binary/findMissingNumberInArray.py
+++ Binary/findMissingNumberInArray.py
@@ -24,19 +24,39 @@ In the given array, number 2 is missing. So, 2 is the answer.
 
 #1. Brute Force Approach
 
-def missing_number(arr, n):
-    # Outer loop that runs from 1 to N:
-    for i in range(1, n + 1):
-        flag = 0
-        for j in range(len(arr)):
-            if arr[j] == i:
-                flag = 1
-                break
-        if flag == 0:
+# def missing_number(arr, n):
+#     # Outer loop that runs from 1 to N:
+#     for i in range(1, n + 1):
+#         flag = 0
+#         for j in range(len(arr)):
+#             if arr[j] == i:
+#                 flag = 1
+#                 break
+#         if flag == 0:
+#             return i
+
+#     return -1
+# n = 5
+# arr = [1, 2, 4, 5]
+# ans = missing_number(arr, n)
+# print("The missing number is:", ans)
+
+
+#1. Better Approach
+def missingNumber(a, N):
+    hash = [0] * (N + 1)
+
+    for i in range(N - 1):
+        hash[a[i]] += 1
+
+
+    for i in range(1, N + 1):
+        if hash[i] == 0:
             return i
 
     return -1
-n = 5
-arr = [1, 2, 4, 5]
-ans = missing_number(arr, n)
-print("The missing number is:", ans)
+
+N = 9
+a = [1, 2, 3, 4, 5, 6, 8, 9,]
+ans = missingNumber(a, N)
+print("-------The missing number is:", ans)
